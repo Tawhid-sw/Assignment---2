9deeb72c3f101f2a2429b4921807830c7036ddce
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-05-24 23:01:28 +0600

    switch bcrypt to bcryptjs cause server crash ( get help by gemini )

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { pool } from "../../db";
 import type { signupProps, loginProps } from "./auth.interface";

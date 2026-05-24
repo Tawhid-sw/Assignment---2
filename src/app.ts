539cbda225a47e3e4a2722710a7fb4179f02ff28
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-05-24 23:06:47 +0600

    added dot env and cors in app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import authRouter from "./modules/auth/auth.routes";
 import issuesRouter from "./modules/issues/issues.routes";
 import errorHandler from "./middleware/error.middleware";
 
+dotenv.config();
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use("/api/auth", authRouter);
+app.use("/api/issues", issuesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Route not found" });
 });
 
-app.use("/api/auth", authRouter);
-app.use("/api/issues", issuesRouter);
-
-// global error handler
 app.use(errorHandler);
 
 export default app;

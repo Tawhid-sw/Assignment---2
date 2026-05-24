5a4cde70d0f49697f24717cdade8381296258403
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-05-24 23:25:09 +0600

    .

--- src/modules/issues/issues.routes.ts
+++ src/modules/issues/issues.routes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
+import {
+  createIssue,
+  getAllIssues,
+  getSingleIssue,
+  updateIssue,
+  deleteIssue,
+} from "./issues.controller";
+import {
+  verifyToken,
+  requireMaintainer,
+} from "../../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("issues");
-});
+router.get("/", getAllIssues);
+router.get("/:id", getSingleIssue);
+
+router.post("/", verifyToken, createIssue);
+router.patch("/:id", verifyToken, updateIssue);
+
+router.delete("/:id", verifyToken, requireMaintainer, deleteIssue);
 
 export default router;

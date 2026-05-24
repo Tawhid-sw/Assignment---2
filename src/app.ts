a45497f1ae1e4e6266e11c3c1ee93086b292431b
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-05-24 23:03:37 +0600

    add an extra routs to check the app

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Route not found" });
 });
